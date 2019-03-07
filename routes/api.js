6321aac6200ec93164be5c0ba90f12723e942a0d
Mostafa Salama <mostafalearning2016@gmail.com>
2019-03-07 15:22:39 +0200

    add create user route

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,17 @@
-const router = require('express').Router() 
+const router = require('express').Router();
+const User = require('../Models/User');
+router.post('/exercise/new-user', (request, response) => {
+	const { username } = request.body;
+	const user = new User({
+		username,
+		exercises: [],
+	});
+	user.save().then(
+		response.json({
+			username: user.username,
+			id: user.id,
+		}),
+	);
+});
 
-router.post('/exercise/new-user',(request,response)=>{
-    console.log(request.body);
-    response.send('hEllo world') ; 
-})
-
-module.exports = router ; 
+module.exports = router;
