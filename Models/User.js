ff695654d34ed21457542500209a02d34dea971a
Mostafa Salama <mostafalearning2016@gmail.com>
2019-03-07 15:13:35 +0200

    add the User Model

--- Models/User.js
+++ Models/User.js
@@ -2,9 +2,22 @@ const shortId = require('shortid') ;
 const mongoose = require('mongoose') ; //#endregion
 
 const UserSchema = mongoose.Schema({
-    name : String ,
+    username : String ,
     _id :{
         type:String,
         default :shortId.generate
-    }
+    },
+    exercises:[
+        {
+            description:String,
+            duration:Number,
+            date:{
+                default:Date.now,
+                type:Date
+            }
+        }
+    ]
+    
 })
+const User = mongoose.model('User',UserSchema);
+module.exports = User ; 
